d3e27f02a57329f2e28377060d98b7519af3d02a
Taran Pierce <taran.tpdesign@gmail.com>
2016-02-21 13:35:34 -0600

    master
    Added the shine theme

---
+++ opencart-themes/shine/Opencart_Files_2.0.3.1/Opencart_Files_2.0.3.1/catalog/controller/common/header.php
@@ -0,0 +1,202 @@
+<?php
+class ControllerCommonHeader extends Controller {
+	public function index() {
+		$data['title'] = $this->document->getTitle();
+
+		if ($this->request->server['HTTPS']) {
+			$server = $this->config->get('config_ssl');
+		} else {
+			$server = $this->config->get('config_url');
+		}
+
+		$data['base'] = $server;
+		$data['description'] = $this->document->getDescription();
+		$data['keywords'] = $this->document->getKeywords();
+		$data['links'] = $this->document->getLinks();
+		$data['styles'] = $this->document->getStyles();
+		$data['scripts'] = $this->document->getScripts();
+		$data['lang'] = $this->language->get('code');
+		$data['direction'] = $this->language->get('direction');
+		if ($this->config->get('config_google_analytics_status')) {
+			$data['google_analytics'] = html_entity_decode($this->config->get('config_google_analytics'), ENT_QUOTES, 'UTF-8');
+		} else {
+			$data['google_analytics'] = '';
+		}
+		$data['name'] = $this->config->get('config_name');
+
+		if (is_file(DIR_IMAGE . $this->config->get('config_icon'))) {
+			$data['icon'] = $server . 'image/' . $this->config->get('config_icon');
+		} else {
+			$data['icon'] = '';
+		}
+
+		if (is_file(DIR_IMAGE . $this->config->get('config_logo'))) {
+			$data['logo'] = $server . 'image/' . $this->config->get('config_logo');
+		} else {
+			$data['logo'] = '';
+		}
+
+		$this->load->language('common/header');
+
+		$data['text_home'] = $this->language->get('text_home');
+		$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), (isset($this->session->data['wishlist']) ? count($this->session->data['wishlist']) : 0));
+		$data['text_shopping_cart'] = $this->language->get('text_shopping_cart');
+		$data['text_logged'] = sprintf($this->language->get('text_logged'), $this->url->link('account/account', '', 'SSL'), $this->customer->getFirstName(), $this->url->link('account/logout', '', 'SSL'));
+
+		$data['text_account'] = $this->language->get('text_account');
+		$data['text_register'] = $this->language->get('text_register');
+		$data['text_login'] = $this->language->get('text_login');
+		$data['text_order'] = $this->language->get('text_order');
+		$data['text_transaction'] = $this->language->get('text_transaction');
+		$data['text_download'] = $this->language->get('text_download');
+		$data['text_logout'] = $this->language->get('text_logout');
+		$data['text_checkout'] = $this->language->get('text_checkout');
+		$data['text_category'] = $this->language->get('text_category');
+		$data['text_all'] = $this->language->get('text_all');
+		$data['text_blog'] = $this->language->get('text_blog');
+		$data['all_blogs'] = $this->url->link('information/blogger/blogs');
+
+		$data['home'] = $this->url->link('common/home');
+		$data['wishlist'] = $this->url->link('account/wishlist', '', 'SSL');
+		$data['logged'] = $this->customer->isLogged();
+		$data['account'] = $this->url->link('account/account', '', 'SSL');
+		$data['register'] = $this->url->link('account/register', '', 'SSL');
+		$data['login'] = $this->url->link('account/login', '', 'SSL');
+		$data['order'] = $this->url->link('account/order', '', 'SSL');
+		$data['transaction'] = $this->url->link('account/transaction', '', 'SSL');
+		$data['download'] = $this->url->link('account/download', '', 'SSL');
+		$data['logout'] = $this->url->link('account/logout', '', 'SSL');
+		$data['shopping_cart'] = $this->url->link('checkout/cart');
+		$data['checkout'] = $this->url->link('checkout/checkout', '', 'SSL');
+		$data['contact'] = $this->url->link('information/contact');
+		$data['telephone'] = $this->config->get('config_telephone');
+		$data['mytemplate'] = $this->config->get('config_template');
+
+		$status = true;
+
+		if (isset($this->request->server['HTTP_USER_AGENT'])) {
+			$robots = explode("\n", str_replace(array("\r\n", "\r"), "\n", trim($this->config->get('config_robots'))));
+
+			foreach ($robots as $robot) {
+				if ($robot && strpos($this->request->server['HTTP_USER_AGENT'], trim($robot)) !== false) {
+					$status = false;
+
+					break;
+				}
+			}
+		}
+		
+		//Blog 
+		
+		$type="module";
+
+		$this->load->model('extension/extension');
+		
+		$result=$this->model_extension_extension->getExtensions($type);
+		
+		foreach($result as $result){
+		if($result['code']==="blogger"){
+		$data['blog_enable'] =1;
+		  }
+		}
+
+		// Menu
+		$this->load->model('catalog/category');
+
+		$this->load->model('catalog/product');
+
+		$data['categories'] = array();
+
+		$categories_1 = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories_1 as $category_1) {
+			if ($category_1['top']) {
+				// Level 2
+				$level_2_data = array();
+
+				$categories_2 = $this->model_catalog_category->getCategories($category_1['category_id']);
+
+			
+				
+				foreach ($categories_2 as $category_2) {
+					$filter_data = array(
+						'filter_category_id'  => $category_2['category_id'],
+						'filter_sub_category' => true
+					);					
+					// Level 2
+					$categories_3 = $this->model_catalog_category->getCategories($category_2['category_id']);
+					$level_3_data = array();
+					foreach ($categories_3 as $category_3) {
+							$data_level2 = array(
+									'filter_category_id'  => $category_3['category_id'],
+									'filter_sub_category' => true
+							);
+							//$product_total_level2 = '';
+//							if ($this->config->get('config_product_count')) {
+//									$product_total_level2 = ' (' . $this->model_catalog_product->getTotalProducts($data_level2) . ')';
+//							}
+
+							$level_3_data[] = array(
+//						'name'  => $child['name'] . ' (' . $product_total . ')', 
+						'name' => $category_3['name'],
+						'href' => $this->url->link('product/category', 'path=' . $category_1['category_id'] . '_' . $category_2['category_id'] . '_' . $category_3['category_id'])
+					);
+				}
+							$level_2_data[] = array(
+							'name'  => $category_2['name'],
+							'children' => $level_3_data,
+							'column'   => $category_2['column'] ? $category_2['column'] : 1,
+							'href'     => $this->url->link('product/category', 'path=' . $category_1['category_id'] . '_' . $category_2['category_id'])	
+					);
+					
+				}
+		
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category_1['name'],
+					'children' => $level_2_data,
+					'column'   => $category_1['column'] ? $category_1['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category_1['category_id']),
+					'id' => $category_1['category_id']
+				);
+			}
+		}
+
+		$data['language'] = $this->load->controller('common/language');
+		$data['currency'] = $this->load->controller('common/currency');
+		$data['search'] = $this->load->controller('common/search');
+		$data['cart'] = $this->load->controller('common/cart');
+		$data['headertopleft'] = $this->load->controller('common/headertopleft');
+		$data['headertopright'] = $this->load->controller('common/headertopright');
+		$data['headernav'] = $this->load->controller('common/headernav');
+		$data['headerbottom'] = $this->load->controller('common/headerbottom');
+
+		// For page specific css
+		if (isset($this->request->get['route'])) {
+			if (isset($this->request->get['product_id'])) {
+				$class = '-' . $this->request->get['product_id'];
+			} elseif (isset($this->request->get['path'])) {
+				$class = '-' . $this->request->get['path'];
+			} elseif (isset($this->request->get['manufacturer_id'])) {
+				$class = '-' . $this->request->get['manufacturer_id'];
+			} else {
+				$class = '';
+			}
+
+			$data['class'] = str_replace('/', '-', $this->request->get['route']) . $class;
+		} else {
+			$data['class'] = 'common-home';
+		}
+		
+		
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');		
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/header.tpl')) {
+			return $this->load->view($this->config->get('config_template') . '/template/common/header.tpl', $data);
+		} else {
+			return $this->load->view('default/template/common/header.tpl', $data);
+		}
+		
+	}
+}
---
+++ opencart-themes/shine/Opencart_Files_2.0.3.1/Opencart_Files_2.0.3.1/catalog/language/arabic/checkout/reward.php
@@ -0,0 +1,17 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title'] = 'استخدم نقاط المكافآت (%s)';
+
+// Text
+$_['text_success']  = 'تم قبول خصم نقاط المكافآت!';
+
+// Entry
+$_['entry_reward']  = 'الحد الأقصى للاستخدام (%s)';
+
+// Error
+$_['error_reward']  = 'تحذير: الرجاء ادخال عدد نقاط المكافآت المطلوب استخدامها !';
+$_['error_points']  = 'تحذير: لايوجد لديك %s نقاط!';
+$_['error_maximum'] = 'تحذير: أقصى حد لاستخدام النقاط هو %s!';
