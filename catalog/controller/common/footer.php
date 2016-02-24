aa14346f1efdd5223e5778bbfb562701179db60e
Taran Pierce <taran.tpdesign@gmail.com>
2016-02-23 21:39:15 -0600

    master
    getting right version of opencart for theme

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -1,100 +1,110 @@
 <?php
 class ControllerCommonFooter extends Controller {
-    public function index() {
-        $this->load->language('common/footer');
+	public function index() {
+		$this->load->language('common/footer');
 
-        $data['scripts'] = $this->document->getScripts('footer');
-        $data['text_information'] = $this->language->get('text_information');
-        $data['text_service'] = $this->language->get('text_service');
-        $data['text_extra'] = $this->language->get('text_extra');
-        $data['text_contact'] = $this->language->get('text_contact');
-        $data['text_return'] = $this->language->get('text_return');
-        $data['text_sitemap'] = $this->language->get('text_sitemap');
-        $data['text_manufacturer'] = $this->language->get('text_manufacturer');
-        $data['text_voucher'] = $this->language->get('text_voucher');
-        $data['text_affiliate'] = $this->language->get('text_affiliate');
-        $data['text_special'] = $this->language->get('text_special');
-        $data['text_account'] = $this->language->get('text_account');
-        $data['text_order'] = $this->language->get('text_order');
-        $data['text_wishlist'] = $this->language->get('text_wishlist');
-        $data['text_newsletter'] = $this->language->get('text_newsletter');
+		$data['scripts'] = $this->document->getScripts('footer');
 
-        $this->load->model('catalog/information');
-        $data['informations'] = array();
+		$data['text_information'] = $this->language->get('text_information');
+		$data['text_service'] = $this->language->get('text_service');
+		$data['text_extra'] = $this->language->get('text_extra');
+		$data['text_contact'] = $this->language->get('text_contact');
+		$data['text_return'] = $this->language->get('text_return');
+		$data['text_sitemap'] = $this->language->get('text_sitemap');
+		$data['text_manufacturer'] = $this->language->get('text_manufacturer');
+		$data['text_voucher'] = $this->language->get('text_voucher');
+		$data['text_affiliate'] = $this->language->get('text_affiliate');
+		$data['text_special'] = $this->language->get('text_special');
+		$data['text_account'] = $this->language->get('text_account');
+		$data['text_order'] = $this->language->get('text_order');
+		$data['text_wishlist'] = $this->language->get('text_wishlist');
+		$data['text_newsletter'] = $this->language->get('text_newsletter');
 
-        foreach ($this->model_catalog_information->getInformations() as $result) {
-            if ($result['bottom']) {
-                $data['informations'][] = array(
-                    'title' => $result['title'],
-                    'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
-                );
-            }
-        }
+		$this->load->model('catalog/information');
+
+		$data['informations'] = array();
+
+		foreach ($this->model_catalog_information->getInformations() as $result) {
+			if ($result['bottom']) {
+				$data['informations'][] = array(
+					'title' => $result['title'],
+					'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
+				);
+			}
+		}
 
 		$data['contact'] = $this->url->link('information/contact');
-        $data['return'] = $this->url->link('account/return/add', '', true);
-        $data['sitemap'] = $this->url->link('information/sitemap');
-        $data['manufacturer'] = $this->url->link('product/manufacturer');
-        $data['voucher'] = $this->url->link('account/voucher', '', true);
-        $data['affiliate'] = $this->url->link('affiliate/account', '', true);
-        $data['special'] = $this->url->link('product/special');
-        $data['account'] = $this->url->link('account/account', '', true);
-        $data['order'] = $this->url->link('account/order', '', true);
-        $data['wishlist'] = $this->url->link('account/wishlist', '', true);
-        $data['newsletter'] = $this->url->link('account/newsletter', '', true);
+		$data['return'] = $this->url->link('account/return/add', '', 'SSL');
+		$data['sitemap'] = $this->url->link('information/sitemap');
+		$data['manufacturer'] = $this->url->link('product/manufacturer');
+		$data['voucher'] = $this->url->link('account/voucher', '', 'SSL');
+		$data['affiliate'] = $this->url->link('affiliate/account', '', 'SSL');
+		$data['special'] = $this->url->link('product/special');
+		$data['account'] = $this->url->link('account/account', '', 'SSL');
+		$data['order'] = $this->url->link('account/order', '', 'SSL');
+		$data['wishlist'] = $this->url->link('account/wishlist', '', 'SSL');
+		$data['newsletter'] = $this->url->link('account/newsletter', '', 'SSL');
 
-        $data['powered'] = sprintf($this->language->get('text_powered'), $this->config->get('config_name'), date('Y', time()));
-
+		$data['powered'] = sprintf($this->language->get('text_powered'), $this->config->get('config_name'), date('Y', time()));
+		
+		
 		$data['footerbottom'] = $this->load->controller('common/footerbottom');
-        $data['footerleft'] = $this->load->controller('common/footerleft');
-        $data['footerright'] = $this->load->controller('common/footerright');
-
+		$data['footerleft'] = $this->load->controller('common/footerleft');
+		$data['footerright'] = $this->load->controller('common/footerright');
+		
 		// Manufacture
-        $this->language->load('product/manufacturer');
-        $this->load->model('catalog/manufacturer');
-        $data['manufacturer_list'] = array();
-        $manufacturers = $this->model_catalog_manufacturer->getManufacturers();
+		$this->language->load('product/manufacturer');
 
-        foreach ($manufacturers as $manufacturer_list) {
-            $data['manufacturer_list'][] = array(
-                'name' => $manufacturer_list['name'],
-                'href' => $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer_list['manufacturer_id'])
-            );
-        }
-        //End Manufacure
+		$this->load->model('catalog/manufacturer');
+		
+		$data['manufacturer_list'] = array();
+		
+		$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
+		
+		foreach ($manufacturers as $manufacturer_list) {
+			$data['manufacturer_list'][] = array(
+				'name' => $manufacturer_list['name'],
+				'href' => $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer_list['manufacturer_id'])
+			);
+		}
+		//End Manufacure
+		
 
-        // Whos Online
-        if ($this->config->get('config_customer_online')) {
-            $this->load->model('tool/online');
+		// Whos Online
+		if ($this->config->get('config_customer_online')) {
+			$this->load->model('tool/online');
 
-            if (isset($this->request->server['REMOTE_ADDR'])) {
-                $ip = $this->request->server['REMOTE_ADDR'];
-            } else {
-                $ip = '';
-            }
+			if (isset($this->request->server['REMOTE_ADDR'])) {
+				$ip = $this->request->server['REMOTE_ADDR'];
+			} else {
+				$ip = '';
+			}
 
-            if (isset($this->request->server['HTTP_HOST']) && isset($this->request->server['REQUEST_URI'])) {
-                $url = 'http://' . $this->request->server['HTTP_HOST'] . $this->request->server['REQUEST_URI'];
-            } else {
-                $url = '';
-            }
+			if (isset($this->request->server['HTTP_HOST']) && isset($this->request->server['REQUEST_URI'])) {
+				$url = 'http://' . $this->request->server['HTTP_HOST'] . $this->request->server['REQUEST_URI'];
+			} else {
+				$url = '';
+			}
 
-            if (isset($this->request->server['HTTP_REFERER'])) {
-                $referer = $this->request->server['HTTP_REFERER'];
-            } else {
-                $referer = '';
-            }
+			if (isset($this->request->server['HTTP_REFERER'])) {
+				$referer = $this->request->server['HTTP_REFERER'];
+			} else {
+				$referer = '';
+			}
 
-            $this->model_tool_online->addOnline($ip, $this->customer->getId(), $url, $referer);
-        }
+			$this->model_tool_online->addOnline($ip, $this->customer->getId(), $url, $referer);
+		}
+		
+		$data['footerbottom'] = $this->load->controller('common/footerbottom');
+		$data['footerleft'] = $this->load->controller('common/footerleft');
+		$data['footerright'] = $this->load->controller('common/footerright');
 
-		// TODO here is the issue, same place as last time 
+
+
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/footer.tpl')) {
-            return $this->load->view($this->config->get('config_template') . '/template/common/footer.tpl', $data);
-        } else {
-			// TODO fixed: the way they referenced the default header was broken
-            //return $this->load->view('default/template/common/footer.tpl', $data);
-			return $this->load->view('common/footer', $data);
-        }
-    }
+			return $this->load->view($this->config->get('config_template') . '/template/common/footer.tpl', $data);
+		} else {
+			return $this->load->view('default/template/common/footer.tpl', $data);
+		}
+	}
 }
